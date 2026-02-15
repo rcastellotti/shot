9afba454a67c5a99f132f5c4555b1e8d18baca7c
Roberto Castellotti <rc@bearbook.tail7a7655.ts.net>
2026-02-15 19:14:36 +0100

    add testdata

---
+++ testdata/scripts.sh
@@ -0,0 +1,8 @@
+```sh
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+curl -fsSL https://deno.land/install.sh | sh
+curl -fsSL https://bun.sh/install | bash
+curl -fsSL https://claude.ai/install.sh | bash
+curl -LsSf https://astral.sh/uv/install.sh | sh
+curl -sS https://starship.rs/install.sh | sh
+```
